40937578693601fcad5335c2c23ffd8f9ae52029
mihzms <millena.marcondes.souza@escola.pr.gov.br>
2025-03-21 09:50:38 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoAumentarFonte = document.getElementById('aumentar-fonte')
+    //Pega o botão de aumentar fonte e coloca na constante
+
+    let tamanhoDaFonte = 1
+    // define o tamanho da fonte na variável
+
+    botaoAumentarFonte.addEventListener('click', function(){
+        tamanhoDaFonte = tamanhoDaFonte + 0.1
+        document.body.style.fontSize = `${tamanhoDaFonte}rem`
+    })
+
+    const botaoDiminuirFonte = document.getElementById('diminir-fonte')
+
+    botaoAumentarFonte.addEventListener('click', function(){
+        tamanhoDaFonte = tamanhoDaFonte - 0.2
+        document.body.style.fontSize = `${tamanhoDaFonte}rem`
+    })
+
+})
